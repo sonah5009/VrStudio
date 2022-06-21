e6c4ca9c889a403f78e817ad7013ded11155884b
sonah5009 <sonah5009@gmail.com>
2022-06-21 14:23:54 +0900

    Upload assignment : TextRPG2

---
+++ TextRPG2/TextRPG2/Creature.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TextRPG2
+{
+    public enum CreatureType
+    {
+        None = 0,
+        Player = 1,
+        Monster = 2
+    }
+    class Creature
+    {
+        CreatureType type;
+
+        protected int hp = 0;
+        protected int attack = 0;
+
+        protected Creature(CreatureType type)
+        {
+            this.type = type;
+        }
+
+        public void SetInfo(int hp, int attack)
+        {
+            this.hp = hp;
+            this.attack = attack;
+        }
+
+        public int GetHp() { return hp; }
+        public int GetAttack() { return attack; }
+        public void OnDamaged(int damage)
+        {
+            hp -= damage;
+            if (hp < 0)
+                hp = 0;
+        }
+
+        public bool IsDead()
+        {
+            return hp <= 0;
+        }
+    }
+}
+
---
+++ TextRPG2/TextRPG2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TextRPG2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Game game = new Game();
+
+            while (true)
+            {
+                game.Process();
+                Console.WriteLine("\n");
+            }
+        }
+    }
+}
